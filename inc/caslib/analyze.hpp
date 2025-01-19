527631c861eefa7b3e5e485e7cf985331bdf5420
BlakeFreer <freerb@mcmaster.ca>
2025-01-18 19:57:49 -0500

    Add doxygen comments.

--- inc/caslib/analyze.hpp
+++ inc/caslib/analyze.hpp
@@ -4,10 +4,33 @@
 
 namespace cas {
 
+/// @brief Calculate the mean of a Series.
+///
+/// @param s
+/// @return float
 float Mean(const Series& s);
+
+/// @brief Calculate the variance of a Series.
+/// @note Uses population (not sample) variance.
+///
+/// @param x
+/// @return float
 float Variance(const Series& x);
+
+/// @brief Calculate the covariance of two Series.
+///
+/// @param x
+/// @param y
+/// @return float
 float Covariance(const Series& x, const Series& y);
 
+/// @brief Calculate the Cross Correlation of two Series at all time lags.
+/// @details Slides `x`, starting with the `x[0]` aligned with `y[last]` until
+/// `x[last]` is aligned with `y[0]`.
+///
+/// @param x
+/// @param y
+/// @return Series
 Series CrossCorrelation(const Series& x, const Series& y);
 
 }  // namespace cas
