dcc2f51982441679128b20eb604d774197e745e0
BlakeFreer <freerb@mcmaster.ca>
2025-01-29 22:02:55 -0500

    Series class. Adds Iterator

--- inc/caslib/series.hpp
+++ inc/caslib/series.hpp
@@ -5,15 +5,71 @@
 
 namespace cas {
 
-using Series = std::vector<float>;
+class Series {
+public:
+    // Constructors
+    Series(float* data, size_t size);
+    Series(std::vector<float> data);
+    Series Clone() const;
 
-Series operator+(const Series& x, const Series& y);
-Series operator-(const Series& x, const Series& y);
-Series operator*(const Series& x, const float scalar);
-Series operator*(const float scalar, const Series& x);
-Series operator-(const Series& x);
-Series operator/(const Series& x, const float divisor);
+    static Series Zeros(size_t size);
+    static Series Ones(size_t size);
 
+    // Accessors
+    size_t Size() const;
+
+    // Operators
+    Series operator+(const Series& y) const;
+    Series operator-(const Series& y) const;
+    Series operator*(const float scalar) const;
+
+    friend Series operator*(const float scalar, const Series& x);
+    Series operator/(const float divisor) const;
+    Series operator-() const;
+    float& operator[](int idx);
+    float operator[](int idx) const;
+
+    friend std::ostream& operator<<(std::ostream& o, const Series& s);
+
+    class Iterator {
+    public:
+        using iterator_category = std::bidirectional_iterator_tag;
+        using value_type = float;
+        using pointer = float*;
+        using reference = float&;
+
+        Iterator(float* p) : p_(p) {}
+        Iterator(const Iterator& it) : p_(it.p_) {}
+        Iterator& operator++() {
+            ++p_;
+            return *this;
+        }
+        Iterator operator++(int) {
+            Iterator tmp(*this);
+            operator++();
+            return tmp;
+        }
+        bool operator==(const Iterator& rhs) const {
+            return p_ == rhs.p_;
+        }
+        bool operator!=(const Iterator& rhs) const {
+            return p_ != rhs.p_;
+        }
+        float& operator*() {
+            return *p_;
+        }
+
+    private:
+        float* p_;
+    };
+
+    Iterator begin();
+    Iterator end();
+
+private:
+    size_t WrapIndex(int idx) const;
+
+    float* data_;
+    size_t size_;
+};
 }  // namespace cas
-
-std::ostream& operator<<(std::ostream& o, const cas::Series& s);
--- src/series.cpp
+++ src/series.cpp
@@ -1,59 +1,121 @@
-#include <cassert>
-#include <ostream>
-
 #include "caslib/series.hpp"
+
+#include <cassert>
+#include <cstring>
+#include <ostream>
 
 namespace cas {
 
-Series operator+(const Series& x, const Series& y) {
-    assert(x.size() == y.size());
+Series::Series(float* data, size_t size) : data_(data), size_(size) {}
 
-    Series output;
-    for (size_t i = 0; i < x.size(); ++i) {
-        output.push_back((x[i] + y[i]));
+Series::Series(std::vector<float> vector) : size_(vector.size()) {
+    // can we take over the vector memory instead of copy it?
+    data_ = (float*)malloc(Size() * sizeof(float));
+
+    for (size_t i = 0; i < Size(); i++) {
+        (*this)[i] = vector[i];
     }
-    return output;
 }
 
-Series operator*(const Series& x, const float scalar) {
-    Series output;
-    for (auto xi : x) {
-        output.push_back(xi * scalar);
+Series Series::Zeros(size_t size) {
+    return Series{(float*)calloc(size, sizeof(float)), size};
+}
+
+Series Series::Ones(size_t size) {
+    Series s{(float*)malloc(size * sizeof(float)), size};
+
+    for (size_t i = 0; i < size; i++) {
+        s[i] = 1;
     }
-    return output;
+    return s;
+}
+
+size_t Series::Size() const {
+    return size_;
+}
+
+Series Series::Clone() const {
+    float* cpy;
+    std::memcpy(&cpy, data_, Size() * sizeof(float));
+    return Series{cpy, Size()};
+}
+
+float& Series::operator[](int idx) {
+    size_t i = WrapIndex(idx);
+    assert(0 <= i && i < Size());
+
+    return data_[i];
+}
+float Series::operator[](int idx) const {
+    size_t i = WrapIndex(idx);
+    assert(0 <= i && i < Size());
+
+    return data_[i];
+}
+
+Series Series::operator+(const Series& y) const {
+    assert(this->size_ == y.size_);
+
+    Series out = Clone();
+    for (size_t i = 0; i < out.Size(); ++i) {
+        out[i] += y[i];
+    }
+    return out;
+}
+
+Series Series::operator*(const float scalar) const {
+    Series out = Clone();
+    for (size_t i = 0; i < out.Size(); i++) {
+        out[i] *= scalar;
+    }
+    return out;
 }
 
 Series operator*(const float scalar, const Series& x) {
     return x * scalar;
 }
 
-Series operator-(const Series& x) {
-    Series output;
-    for (auto xi : x) {
-        output.push_back(-xi);
+Series Series::operator-() const {
+    Series out = Clone();
+    for (size_t i = 0; i < out.Size(); i++) {
+        out[i] = -out[i];
     }
-    return output;
+    return out;
 }
 
-Series operator-(const Series& x, const Series& y) {
-    assert(x.size() == y.size());
+Series Series::operator-(const Series& y) const {
+    assert(Size() == y.Size());
 
-    return x + -y;
+    return *this + -y;
 }
 
-Series operator/(const Series& x, const float divisor) {
-    Series output;
-    for (auto xi : x) {
-        output.push_back(xi / divisor);
+Series Series::operator/(const float divisor) const {
+    Series out = Clone();
+    for (size_t i = 0; i < out.Size(); i++) {
+        out[i] /= divisor;
     }
-    return output;
+    return out;
 }
 
-}  // namespace  cas
-
-std::ostream& operator<<(std::ostream& o, const cas::Series& x) {
-    for (auto xi : x) {
-        o << xi << std::endl;
+std::ostream& operator<<(std::ostream& o, const Series& x) {
+    for (size_t i = 0; i < x.Size(); i++) {
+        o << x[i] << std::endl;
     }
     return o;
 }
+
+Series::Iterator Series::begin() {
+    return Iterator{data_};
+}
+Series::Iterator Series::end() {
+    return Iterator{data_ + size_};
+}
+
+size_t Series::WrapIndex(int idx) const {
+    if (idx < 0) {
+        return size_ + idx;
+    }
+    return idx;
+}
+
+}  // namespace  cas
